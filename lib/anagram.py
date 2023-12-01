948027083745794b61044783b0f470919a751b7a
rasmi <rasminoel@gmail.com>
2023-12-01 13:34:15 +0300

    Done with Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram (object):
+    def __init__(self, word):
+        self.word = word
+        self.letters = sorted(word.lower())
+    def match(self, words):
+        return [w for w in words if self.is_anagram(w)]
+    def is_anagram(self, word):
+        return self.letters == sorted(word.lower()) and self.word.lower() != word.lower()
